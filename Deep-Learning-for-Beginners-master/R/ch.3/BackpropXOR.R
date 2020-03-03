cc58dc1433ada38abc0bbd8622d4d2f3783ecc2a
azxj <1158257471@qq.com>
2020-03-03 09:36:09 +0800

    Upload Deep-Learning-for-Beginners-master/R/ch.3/

---
+++ Deep-Learning-for-Beginners-master/R/ch.3/BackpropXOR.R
@@ -0,0 +1,27 @@
+BackpropXOR <- function(W1, W2, X, D) {
+  alpha <- 0.9
+
+  N <- 4
+  for (k in 1:N) {
+    x <- X[k,]
+    d <- D[k]
+
+    v1 <- W1 %*% x
+    y1 <- Sigmoid(v1)
+    v <- W2 %*% y1
+    y <- Sigmoid(v)
+
+    e <- d - y
+    delta <- y * (1-y) * e
+
+    e1 <- t(W2) %*% delta
+    delta1 <- y1 * (1-y1) * e1
+
+    dW1 <- alpha * delta1 %*% t(x)
+    W1 <- W1 + dW1
+
+    dW2 <- alpha * delta %*% t(y1)
+    W2 <- W2 + dW2
+  }
+  return(list("W1"=W1, "W2"=W2))
+}
---
+++ Deep-Learning-for-Beginners-master/R/ch.3/TestBackpropCE.R
@@ -0,0 +1,32 @@
+X <- matrix(c(
+  0, 0, 1,
+  0, 1, 1,
+  1, 0, 1,
+  1, 1, 1
+), nrow=4, ncol=3, byrow=TRUE)
+
+D <- c(
+  0,
+  1,
+  1,
+  0
+)
+
+W1 <- matrix(runif(12, min=-1, max=1), nrow=4, ncol=3)
+W2 <- matrix(runif(4, min=-1, max=1), nrow=1, ncol=4)
+
+for (epoch in 1:10000) { # train
+  Ws <- BackpropCE(W1, W2, X, D)
+  W1 <- Ws$W1
+  W2 <- Ws$W2
+}
+
+N <- 4 # inference
+for (k in 1:N) {
+  x <- X[k,]
+  v1 <- W1 %*% x
+  y1 <- Sigmoid(v1)
+  v <- W2 %*% y1
+  y <- Sigmoid(v)
+  print(y)
+}
