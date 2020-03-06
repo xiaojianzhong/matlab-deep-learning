b0fc6d6fd2b580222a3e8ab2385689e22c7f7ec9
azxj <1158257471@qq.com>
2020-03-07 00:48:07 +0800

    Add comments for chapter 6

--- Deep-Learning-for-Beginners-master/Matlab/ch.6/PlotFeatures.m
+++ Deep-Learning-for-Beginners-master/Matlab/ch.6/PlotFeatures.m
@@ -1,21 +1,23 @@
+% 该文件展示了在正向传播的过程中图像是如何一步步被处理的。
+
 clear all
 
-load('MnistConv.mat')
+load('MnistConv.mat') % 加载预训练模型
 
-k  = 2;
+k  = 2; % 取训练集中第 2 张图片作为例子进行说明
 x  = X(:, :, k);
-y1 = Conv(x, W1);                 % Convolution,  20x20x20
-y2 = ReLU(y1);                    %
-y3 = Pool(y2);                    % Pool,         10x10x20
-y4 = reshape(y3, [], 1);          %                   2000
-v5 = W5*y4;                       % ReLU,              360
-y5 = ReLU(v5);                    %
-v  = Wo*y5;                       % Softmax,            10
-y  = Softmax(v);                  %
+y1 = Conv(x, W1);
+y2 = ReLU(y1);
+y3 = Pool(y2);
+y4 = reshape(y3, [], 1);
+v5 = W5*y4;
+y5 = ReLU(v5);
+v  = Wo*y5;
+y  = Softmax(v);
 
 
 figure;
-display_network(x(:));
+display_network(x(:)); % 显示输入的图片
 title('Input Image')
 
 convFilters = zeros(9*9, 20);
@@ -24,13 +26,13 @@ for i = 1:20
   convFilters(:, i) = filter(:);
 end
 figure
-display_network(convFilters);
+display_network(convFilters); % 显示卷积层用到的过滤器
 title('Convolution Filters')
 
 fList = zeros(20*20, 20);
 for i = 1:20
   feature     = y1(:, :, i);
-  fList(:, i) = feature(:);
+  fList(:, i) = feature(:); % 显示图片经过卷积处理后的结果
 end
 figure
 display_network(fList);
@@ -42,7 +44,7 @@ for i = 1:20
   fList(:, i) = feature(:);
 end
 figure
-display_network(fList);
+display_network(fList); % 显示图片经过卷积 + ReLU 处理后的结果
 title('Features [Convolution + ReLU]')
 
 fList = zeros(10*10, 20);
@@ -51,5 +53,5 @@ for i = 1:20
   fList(:, i) = feature(:);
 end
 figure
-display_network(fList);
+display_network(fList); % 显示图片经过卷积 + ReLU + 池化处理后的结果
 title('Features [Convolution + ReLU + MeanPool]')
