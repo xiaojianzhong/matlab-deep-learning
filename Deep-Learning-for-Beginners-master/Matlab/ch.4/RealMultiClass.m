6431db1d8efde7626785dd9a3be9af2af35e4b40
azxj <1158257471@qq.com>
2020-03-06 16:40:27 +0800

    Add comments for chapter 4

--- Deep-Learning-for-Beginners-master/Matlab/ch.4/RealMultiClass.m
+++ Deep-Learning-for-Beginners-master/Matlab/ch.4/RealMultiClass.m
@@ -1,6 +1,10 @@
+% 该文件使用（未在训练集中出现过的）真实数据对 MultiClass 函数进行了测试。
+% 这样的测试是为了检验所训练出来的模型的泛化能力。
+
 clear all
 
-TestMultiClass;                 % W1, W2
+TestMultiClass; % 调用 TestMultiClass.m 来训练模型
+                % 我们可以使用训练好的模型的权重参数 W1 和 W2 来进行预测
 
 X  = zeros(5, 5, 5);
 
@@ -39,7 +43,8 @@ X(:, :, 5) = [ 0 1 1 1 1;
                1 1 1 1 0
              ];
 
-N = 5;                        % inference
+% 使用模型进行预测
+N = 5;
 for k = 1:N
   x  = reshape(X(:, :, k), 25, 1);
   v1 = W1*x;
--- Deep-Learning-for-Beginners-master/Matlab/ch.4/rng.m
+++ Deep-Learning-for-Beginners-master/Matlab/ch.4/rng.m
@@ -1,3 +1,6 @@
+% rng 函数设置随机数发生器的种子。
+%
+% x 是一个标量。
 function rng(x)
   randn('seed', x)
   rand('seed', x)
