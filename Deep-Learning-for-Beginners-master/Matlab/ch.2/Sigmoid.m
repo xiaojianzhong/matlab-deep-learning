85c1044dcf1d7c0fe7048ebb592401292944590a
azxj <1158257471@qq.com>
2020-03-06 13:54:27 +0800

    Substitute the related link

--- Deep-Learning-for-Beginners-master/Matlab/ch.2/Sigmoid.m
+++ Deep-Learning-for-Beginners-master/Matlab/ch.2/Sigmoid.m
@@ -1,5 +1,5 @@
 % Sigmoid 函数对输入参数执行 sigmoid 变换。
-% sigmoid 是激活函数的一种，关于 sigmoid 的更多信息，可以参考 https://baike.baidu.com/item/Sigmoid%E5%87%BD%E6%95%B0/7981407。
+% sigmoid 是激活函数的一种，关于 sigmoid 的更多信息，可以参考 https://en.wikipedia.org/wiki/Sigmoid_function。
 %
 % x 是一个标量。
 %
