fd2e847541430453248eab7551ddf446fd82ad6a
azxj <1158257471@qq.com>
2020-03-06 13:35:57 +0800

    Correct comments

--- Deep-Learning-for-Beginners-master/Matlab/ch.2/DeltaBatch.m
+++ Deep-Learning-for-Beginners-master/Matlab/ch.2/DeltaBatch.m
@@ -14,6 +14,7 @@ function W = DeltaBatch(W, X, D)
   N = 4; % 训练数据的数量，亦即矩阵 X 的行数量
   for k = 1:N
     x = X(k, :)'; % 一个单独的训练样本的特征向量
+                  % 注意这里进行了转置，将行向量转换为列向量
                   % 3 维向量
     d = D(k); % 该样本的标签
               % 标量
@@ -29,7 +30,7 @@ function W = DeltaBatch(W, X, D)
                        % 标量
 
     dW = alpha*delta*x; % 权重参数的更新值
-                        % 3 维向量（和权重参数相同）
+                        % 3 维向量
 
     dWsum = dWsum + dW;
   end
@@ -37,7 +38,7 @@ function W = DeltaBatch(W, X, D)
                      % 3x1 矩阵（3 维向量）
 
   % 使用平均值来更新权重参数
-  % 这里实际上可以直接使用 W = W + dWavg
+  % 由于 W 是 1x3 矩阵，而 dW 是 3x1 矩阵，故不能直接执行 W = W + dW
   W(1) = W(1) + dWavg(1);
   W(2) = W(2) + dWavg(2);
   W(3) = W(3) + dWavg(3);
